260026a4812a24c85f01cec0ed24b4682f8a4fbd
sjohnnlee <killerlee1992@gmail.com>
2020-04-14 12:50:48 -0700

    blog.css

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -18,7 +18,10 @@ export default class BlogDetail extends Component {
           .currentId}`
       )
       .then(response => {
-        console.log("response", response);
+        
+        this.setState({
+          blogItem:response.data.portfolio_blog
+        })
       })
       .catch(error => {
         console.log("getBlogItem error", error);
@@ -30,9 +33,23 @@ export default class BlogDetail extends Component {
   }
 
   render() {
+    const{
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
     return (
-      <div>
-        <h1>Blog detail</h1>
+      <div className="blog-container">
+        <div className="content-container">
+        <h1>{ title}</h1>
+        <div className="featured-image-wrapper">
+        <img src={featured_image_url}/>
+        </div>
+        <div className="content">
+    <div>{content}</div>
+        </div>
+      </div>
       </div>
     );
   }
