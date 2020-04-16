c45164394ad941332954d53a1b3945f34eed7ef9
sjohnnlee <killerlee1992@gmail.com>
2020-04-16 08:53:42 -0700

    infinite scrolling

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,56 +1,54 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class BlogDetail extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       currentId: this.props.match.params.slug,
-      blogItem: {}
-    };
+      blogItem: {},
+    }
   }
 
   getBlogItem() {
     axios
       .get(
-        `https://sjohnntylerlee.devcamp.space/portfolio/portfolio_blogs/${this.state
-          .currentId}`
+        `https://sjohnntylerlee.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`,
       )
       .then(response => {
-        
         this.setState({
-          blogItem:response.data.portfolio_blog
+          blogItem: response.data.portfolio_blog,
         })
       })
       .catch(error => {
-        console.log("getBlogItem error", error);
-      });
+        console.log('getBlogItem error', error)
+      })
   }
 
   componentDidMount() {
-    this.getBlogItem();
+    this.getBlogItem()
   }
 
   render() {
-    const{
+    const {
       title,
       content,
       featured_image_url,
-      blog_status
-    } = this.state.blogItem;
+      blog_status,
+    } = this.state.blogItem
     return (
       <div className="blog-container">
         <div className="content-container">
-        <h1>{ title}</h1>
-        <div className="featured-image-wrapper">
-        <img src={featured_image_url}/>
-        </div>
-        <div className="content">
-    <div>{content}</div>
+          <h1>{title}</h1>
+          <div className="featured-image-wrapper">
+            <img src={featured_image_url} />
+          </div>
+          <div className="content">
+            <div>{content}</div>
+          </div>
         </div>
       </div>
-      </div>
-    );
+    )
   }
 }
